e280cc9ceefcbd90e308f938029885582d9ee67f
Bernard Lyons <blyons22@msu.idserve.net>
2023-01-24 10:41:29 -0500

    Initial Commit

---
+++ FinancialLiteracyApp/FinancialLiteracyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinancialLiteracyAppApp.swift
+//  FinancialLiteracyApp
+//
+//  Created by Bernard Lyons on 1/24/23.
+//
+
+import SwiftUI
+
+@main
+struct FinancialLiteracyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
